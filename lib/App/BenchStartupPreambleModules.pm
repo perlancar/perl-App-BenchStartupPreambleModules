93c455996f3908de7c93e77faa5a8f224c2c3104
perlancar (@pc-home) <perlancar@gmail.com>
2015-06-14 12:19:26 +0700

    Initial commit

---
+++ lib/App/BenchStartupPreambleModules.pm
@@ -0,0 +1,11 @@
+package App::BenchStartupPreambleModules;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Benchmark startup time of various Perl modules commonly used in code preamble
+
+=head1 SYNOPSIS
+
+See the included script L<bench-startup-preamble-modules>.
